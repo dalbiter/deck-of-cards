d42855b58a723163fc02046ec263134bfc9d639f
danx <rushn24@gmail.com>
2024-08-03 19:30:36 -0400

    able to draw random cards from single deck

--- src/CardWrapper.jsx
+++ src/CardWrapper.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import axios from 'axios';
 
 const CardWrapper = () => {
-    const [card, setCard] = useState({
-        value: 'ace',
-        suit: 'spades'
-    })
+    const [deckId, setDeckId] = useState()
+    const [drawnCards, setDrawnCards] = useState([])
+    const drawCard =  async (deckId) => {
+        const card = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`)
+        setDrawnCards([...drawnCards, {key: card.data.cards[0].code, value: card.data.cards[0].value, suit: card.data.cards[0].suit}])
+    }
+    useEffect(() => {
+        const loadDeck = async () => {
+            const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+            setDeckId(res.data.deck_id)
+        };
+        loadDeck();
+    }, [])
     return (
         <div>
-            <button>Draw Card</button>
-            <Card value={card.value} suit={card.suit} />
+            <button onClick={() => drawCard(deckId)}>Draw Card</button>
+            {drawnCards.map(({ key, value, suit }) => (
+                <Card key ={key} value={value} suit={suit} />
+            ))}
         </div>
     );
 };
