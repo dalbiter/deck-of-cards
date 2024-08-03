1c94c3ee23b07fa587f75f9101e584b43af8276a
danx <rushn24@gmail.com>
2024-08-03 19:55:38 -0400

    added try/catch for simple error handling

--- src/CardWrapper.jsx
+++ src/CardWrapper.jsx
@@ -6,13 +6,22 @@ const CardWrapper = () => {
     const [deckId, setDeckId] = useState()
     const [drawnCards, setDrawnCards] = useState([])
     const drawCard =  async (deckId) => {
-        const card = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`)
-        setDrawnCards([...drawnCards, {key: card.data.cards[0].code, value: card.data.cards[0].value, suit: card.data.cards[0].suit}])
+        try {
+            const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`)
+            setDrawnCards([...drawnCards, {key: res.data.cards[0].code, value: res.data.cards[0].value, suit: res.data.cards[0].suit}])
+        } catch {
+            alert("Error: No cards remaining!")
+        }
+        
     }
     useEffect(() => {
         const loadDeck = async () => {
-            const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
-            setDeckId(res.data.deck_id)
+            try {
+                const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+             setDeckId(res.data.deck_id)
+            } catch {
+                alert("Not Found")
+            }
         };
         loadDeck();
     }, [])
